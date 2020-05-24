1b60a86f73b747a63468cc1992b47565adfa675a
Nick Stokoe <git.wu-lee@noodlefactory.co.uk>
2020-05-24 21:39:01 +0100

    ica-youth-network/test/converter.rb - document, tidy
    Some fields seem mis-categorised as standard fields or internal-use, so
    I've moved them. Additionally, I've prefixed internal-use field names
    with an underscore.
    
    `description` seemed to miss out some descriptions, corrected
    this. Joined with additional_desc using SubFieldSeparator

--- ica-youth-network/test/converter.rb
+++ ica-youth-network/test/converter.rb
@@ -1,3 +1,4 @@
+# coding: utf-8
 
 
 # This is the Converter for Co-ops UK 'outlets' CSV.
@@ -27,85 +28,109 @@ TYPE_TO_ORG_STRUCT = {
 
 
 
+# Defines a schema mapping for ica-youth-network data.
+#
+# The hash {SpecializedCsvReader::InputHeaders} identifies methods which
+# should be implemented as simple CSV field look-ups.
+#
+# Some are named in {OutputStandard}'s keys and so will be copied
+# verbatim into the output CSV data. These have no underscore prefix.
+#
+# Those with underscore prefixes are used internally.
+#
+# The other keys of {OutputStandard} must also have methods
+# implemented in this class. These are the ones which have more
+# complicated transformation rules - they cannot be a simple copy of
+# an existing data field.
+#
+# Input data fields are:
+#
+# - Organization Type
+# - Name
+# - name
+# - Region
+# - Country
+# - City
+# - Latitude
+# - Longitude
+# - Size
+# - Type
+# - Sector
+# - Address
+# - Description
+# - Additional Details
+# - Website
+# - Email
+#
 class SpecializedCsvReader < SeOpenData::CSV::RowReader
-  # Headers in input CSV (with Hash key symbols matching Hash key symbols in output CSV Headers)
+
+  # These define methods (as keys) to implement as simple hash
+  # look-ups using CSV field names (values).
+  #
   InputHeaders = {
-    # These symbols match symbols in OutputStandard::Headers.
-    # So the corresponding cells with be copied from input to output:
-    name: "",
+    id: "Id",
     # postcode: "PostCode",
     country_name: "Country",
-    organisational_structure: "",
-    type:"Type",
-    description: "",
-    desc: "Description",
-    additional_desc: "Additional Details",
-
-    # These symbols don't match symbols in OutputStandard::Headers,
-    # but CSV::RowReader creates a method using these symbol names to
-    # read that column from the row:
-    
-    #registrar: "Registrar",
-    #registered_number: "Registered Number"
-    mail:"Email",
-    tempName: "Name",
+    type: "Type",
     street_address: "Address",
     locality: "City",
-    country: "Country",
-    homepage: "",
-    website: "Website",
-    id: "Id",
-    lt:"Latitude",
-    ln:"Longitude",
-    longitude: "",
-    latitude: "",
-    email:""
-  }
 
+    #registrar: "Registrar",
+    #registered_number: "Registered Number"
+    _desc: "Description",
+    _additional_desc: "Additional Details",
+    _mail: "Email",
+    _name: "Name",
+    _country: "Country",
+    _website: "Website",
+    _lt: "Latitude",
+    _ln: "Longitude",
+  }
+  # MISSING!
+  # primary_activity (name?)
+  # activities
+  # region (region?) 
+  # postcode
+  # phone
+  # twitter
+  # facebook
+  # companies_house_number
+  # geocontainer
+  # geocontainer_lat
+  # geocontainer_lon
+  
+  # @param row [Hash<String => String>] - a row of CSV data
   def initialize(row)
     # Let CSV::RowReader provide methods for accessing columns described by InputHeaders, above:
     super(row, InputHeaders)
   end
-  # Some columns in the output are not simple copies of input columns:
-  # Here are the methods for generating those output columns:
-  # (So all method names below should aldo appear as keys in the output_headers Hash)
-  # def id
-  #   raise(SeOpenData::Exception::IgnoreCsvRow, "\"Domain\" column is empty") unless domain
-  #   domain.sub(/\.coop$/, "")
-  # end
+
 
   def longitude
-    if LAT_LNG_REGEX.match?(ln)
-      ln
+    if LAT_LNG_REGEX.match?(_ln)
+      _ln
     else
       0.0
     end
   end
+  
   def latitude
-    if LAT_LNG_REGEX.match?(lt)
-      lt
+    if LAT_LNG_REGEX.match?(_lt)
+      _lt
     else
       0.0
     end
   end
 
   def description
-    
-    descript = ""
-    if desc
-      descript += desc
-    end
-    if additional_desc
-      descript += additional_desc
-    end
-
+    [_desc, _additional_desc].compact.join("\\n\\n")
   end
   
   def email
-    if mail && !mail.empty?
-      e = EMAIL_REGEX.match?(mail)
+    if _mail && !_mail.empty?
+      e = EMAIL_REGEX.match?(_mail)
       if e
-        return mail
+        return _mail
       else
         return ""
       end
@@ -115,18 +140,18 @@ class SpecializedCsvReader < SeOpenData::CSV::RowReader
   end
 
   def homepage
-    if website && !website.empty?
+    if _website && !_website.empty?
       http_regex = /https?\S+/
-      m = http_regex.match(website)
+      m = http_regex.match(_website)
       if m
         m[0]
       else
         www_regex =  /^www\./
-        www_m = www_regex.match(website)
+        www_m = www_regex.match(_website)
         if www_m
-          "http://#{website}"
+          "http://#{_website}"
         else
-          add_comment("This doesn't look like a website: #{website} (Maybe it's missing the http:// ?)")
+          add_comment("This doesn't look like a website: #{_website} (Maybe it's missing the http:// ?)")
           nil
         end
       end
@@ -134,20 +159,14 @@ class SpecializedCsvReader < SeOpenData::CSV::RowReader
   end
 
   def name
-    if(tempName)
-      tempName
-    else
-      "No Name"
-    end
+    _name || "No Name"
   end
 
   def legal_forms
     # Return a list of strings, separated by OutputStandard::SubFieldSeparator.
     # Each item in the list is a prefLabel taken from essglobal/standard/legal-form.skos.
     # See lib/se_open_data/essglobal/legal_form.rb
-    [
-      "Coopecountry_namerative"
-    ].compact.join(OutputStandard::SubFieldSeparator)
+    "Cooperative"
   end
 
   def organisational_structure
@@ -156,9 +175,7 @@ class SpecializedCsvReader < SeOpenData::CSV::RowReader
     ## See lib/se_open_data/essglobal/legal_form.rb
     org_st = TYPE_TO_ORG_STRUCT[type]? TYPE_TO_ORG_STRUCT[type] : "Co-operative"
 
-    [
-      org_st
-    ].compact.join(OutputStandard::SubFieldSeparator)
+    org_st
   end
   
 end
