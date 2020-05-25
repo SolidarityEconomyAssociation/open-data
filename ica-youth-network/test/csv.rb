04890b7c2638e64efafd09fa69fd521b196ea0df
Nick Stokoe <git.wu-lee@noodlefactory.co.uk>
2020-05-25 23:30:24 +0100

    ica-youth-network/test/clear_csv_errors.rb - merge into csv.rb
    Avoids shelling out, somewhat simpler.
    
    Tweak a few things, like removing unused variables, ensure IO streams
    are closed.

--- ica-youth-network/test/csv.rb
+++ ica-youth-network/test/csv.rb
@@ -19,14 +19,45 @@ cleared_errors = File.join(GEN_DIR, "cleared_errors.csv")
 output_csv = $config_map["STANDARD_CSV"]
 
 
+# Performs the following on the input CSV stream
+#
+# - remove all single quotes not followed by a quote 
+# - replace ' with empty
+# - replace double quotes with single quote
+# - make sure to place quote before the last two commas
+#
+# @param in_f [IO] - an IO stream reading a CSV document
+# @param out_f [IO] - an IO stream writing the new CSV document
 def clear_csv_errors(in_f:, out_f:)
-  Config.gen_ruby_command(
-    in_f,
-    "clear_csv_errors.rb",
-    nil,
-    out_f,
-    nil
-  )  
+  File.open(in_f) do |text|
+    File.open(out_f, 'w') do |csv_out|
+  
+      error_detected = false
+      headers = nil
+      count = 0
+      
+      text.each_line do |line|
+        if !headers # if there's an error in the headers there's an error in the file
+          headers = line
+          if line.include? "\""
+            error_detected = true
+          end
+        end
+        if error_detected
+          line.encode!('UTF-8', 'UTF-8', :invalid => :replace)
+          line.delete!("\xEF\xBB\xBF")
+          line = line.sub('"','').sub(/.*\K\"/, '').gsub("'","").
+                   gsub("\"\"","replaceMeWithQuote").gsub("\"","").gsub("replaceMeWithQuote","\"")
+          csv_out.print(line) 
+        # line = line.sub(/.*\K\"/, '')
+        # strm = ","+line.split(',')[-2] +","+ line.split(',')[-1]
+        # line.insert(line.index(strm),"\"")
+        else
+          csv_out.print(line)
+        end
+      end
+    end
+  end
 end
 
 def add_unique_ids(in_f:, out_f:)
