2727bac5025702865c111b8ca325cb330f66687e
Nick Stokoe <git.wu-lee@noodlefactory.co.uk>
2020-05-26 23:13:24 +0100

    ica-youth-network/test/load_config.rb - modify to match the expected output
    So, make local paths absolute. Keep remote paths as-is. Add a trailing
    slash on directories, as was the case before, just to be on the safe
    side: some logic downstream may be relying on it.

--- ica-youth-network/test/load_config.rb
+++ ica-youth-network/test/load_config.rb
@@ -9,7 +9,8 @@ module SeOpenData
   # This is an abstraction layer from the config file itself.
   # i.e. Variables here are independant from names in the config file.
   class Config
-
+    require 'fileutils'
+    
     # @param file [String] - the path to the config file to load.
     # @param base_dir [String] - the base directory in which to locate certain paths
     def initialize(file, base_dir = Config.caller_dir)
@@ -25,31 +26,45 @@ module SeOpenData
 
       # setup Config
       # setup lib path, this needs to be changed
-      conf["SE_OPEN_DATA_LIB_DIR"] = File.expand_path(conf["SE_OPEN_DATA_LIB_DIR"], base_dir)
-      conf["SE_OPEN_DATA_BIN_DIR"] = File.expand_path(conf["SE_OPEN_DATA_BIN_DIR"], base_dir) + "/"
 
       # csv.rb
+      def join(*args)  # joins using local path delimiter
+        File.join(*args)
+      end
+      def unixjoin(first, *rest) # uses the unix '/' delimiter
+        #First part must have trailing slash removed only, rest must
+        # have (a single) leading slash.
+        first.gsub(%r{/+$},'')+rest.map {|it| it.gsub(%r{^/*},"/") }.join
+      end
+      
+      # Expand these paths relative to base_dir
+      %w(TOP_OUTPUT_DIR SRC_CSV_DIR CSS_SRC_DIR SE_OPEN_DATA_LIB_DIR SE_OPEN_DATA_BIN_DIR)
+        .each do |key| # expand rel to base_dir, append a slash
+          conf[key] = join File.expand_path(conf[key], base_dir), ""
+        end
+
       # This is the directory where we generate intermediate csv files
-      conf["GEN_CSV_DIR"] = conf["TOP_OUTPUT_DIR"] + "csv/"
+      conf["GEN_CSV_DIR"] = join conf["TOP_OUTPUT_DIR"], "csv", ""
 
       #goal end file (standard.csv)
-      conf["STANDARD_CSV"] = conf["TOP_OUTPUT_DIR"] + conf["STANDARD_CSV"]
+      conf["STANDARD_CSV"] = join conf["TOP_OUTPUT_DIR"], conf["STANDARD_CSV"]
       #csv.rb end
       
       #generate.rb
-      conf["WWW_DIR"] = conf["TOP_OUTPUT_DIR"] + "www/"
-      conf["GEN_DOC_DIR"] = conf["WWW_DIR"] + "doc/"
-      conf["GEN_CSS_DIR"] = conf["GEN_DOC_DIR"]+"css/"
-      conf["GEN_VIRTUOSO_DIR"] = conf["TOP_OUTPUT_DIR"]+"virtuoso/"
-      conf["GEN_SPARQL_DIR"] = conf["TOP_OUTPUT_DIR"]+"sparql/"
-      conf["SPARQL_GET_ALL_FILE"] = conf["GEN_SPARQL_DIR"]+"query.rq"
-      conf["SPARQL_LIST_GRAPHS_FILE"] = conf["GEN_SPARQL_DIR"]+"list-graphs.rq"
-      conf["SPARQL_ENDPOINT_FILE"] = conf["GEN_SPARQL_DIR"]+"endpoint.txt"
-      conf["SPARQL_GRAPH_NAME_FILE"] = conf["GEN_SPARQL_DIR"]+"default-graph-uri.txt"
+      conf["WWW_DIR"] = unixjoin conf["TOP_OUTPUT_DIR"], "www", ""
+      conf["GEN_DOC_DIR"] = unixjoin conf["WWW_DIR"], "doc", ""
+      conf["GEN_CSS_DIR"] = unixjoin conf["GEN_DOC_DIR"], "css", ""
+      conf["GEN_VIRTUOSO_DIR"] = unixjoin conf["TOP_OUTPUT_DIR"], "virtuoso", ""
+      conf["GEN_SPARQL_DIR"] = unixjoin conf["TOP_OUTPUT_DIR"], "sparql", ""
+      conf["SPARQL_GET_ALL_FILE"] = unixjoin conf["GEN_SPARQL_DIR"], "query.rq"
+      conf["SPARQL_LIST_GRAPHS_FILE"] = unixjoin conf["GEN_SPARQL_DIR"], "list-graphs.rq"
+      conf["SPARQL_ENDPOINT_FILE"] = unixjoin conf["GEN_SPARQL_DIR"], "endpoint.txt"
+      conf["SPARQL_GRAPH_NAME_FILE"] = unixjoin conf["GEN_SPARQL_DIR"], "default-graph-uri.txt"
       conf["DATASET_URI_BASE"] = "#{conf["URI_SCHEME"]}://#{conf["URI_HOST"]}/#{conf["URI_PATH_PREFIX"]}"
       conf["GRAPH_NAME"] = conf["DATASET_URI_BASE"]
-      conf["ONE_BIG_FILE_BASENAME"] = "#{conf["GEN_VIRTUOSO_DIR"]}all"
-      conf["CSS_FILES"] =  Dir[conf["CSS_SRC_DIR"]+"*.css"].join(",")
+      conf["ONE_BIG_FILE_BASENAME"] = unixjoin conf["GEN_VIRTUOSO_DIR"], "all"
+      
+      conf["CSS_FILES"] =  Dir[join conf["CSS_SRC_DIR"], "*.css"].join(",")
       conf["SAME_AS_FILE"] = conf.key?("SAMEAS_CSV") ? conf["SAMEAS_CSV"] : "" 
       conf["SAME_AS_HEADERS"] = conf.key?("SAMEAS_HEADERS") ? conf["SAMEAS_HEADERS"] : "" 
 
@@ -57,24 +72,24 @@ module SeOpenData
 
       #deploy.rb
       conf["DEPLOYMENT_DOC_SUBDIR"] = conf["URI_PATH_PREFIX"]
-      conf["DEPLOYMENT_DOC_DIR"] = conf["DEPLOYMENT_WEBROOT"] + conf["DEPLOYMENT_DOC_SUBDIR"]
+      conf["DEPLOYMENT_DOC_DIR"] = unixjoin conf["DEPLOYMENT_WEBROOT"], conf["DEPLOYMENT_DOC_SUBDIR"]
 
       #deploy.rb
 
       #triplestore.rb
-      conf["VIRTUOSO_NAMED_GRAPH_FILE"]=conf["GEN_VIRTUOSO_DIR"]+"global.graph"
-      conf["VIRTUOSO_SQL_SCRIPT"]="loaddata.sql"
+      conf["VIRTUOSO_NAMED_GRAPH_FILE"] = unixjoin conf["GEN_VIRTUOSO_DIR"], "global.graph"
+      conf["VIRTUOSO_SQL_SCRIPT"] = "loaddata.sql"
 
       conf["VERSION"] = make_version
-      conf["VIRTUOSO_DATA_DIR"] = "#{conf["VIRTUOSO_ROOT_DATA_DIR"]}#{conf["VERSION"]}/"
-      conf["VIRTUOSO_SCRIPT_LOCAL"] = conf["GEN_VIRTUOSO_DIR"]+conf["VIRTUOSO_SQL_SCRIPT"]
-      conf["VIRTUOSO_SCRIPT_REMOTE"] = conf["VIRTUOSO_DATA_DIR"] + conf["VIRTUOSO_SQL_SCRIPT"]
+      conf["VIRTUOSO_DATA_DIR"] = unixjoin conf["VIRTUOSO_ROOT_DATA_DIR"], conf["VERSION"], ""
+      conf["VIRTUOSO_SCRIPT_LOCAL"] = join conf["GEN_VIRTUOSO_DIR"], conf["VIRTUOSO_SQL_SCRIPT"]
+      conf["VIRTUOSO_SCRIPT_REMOTE"] = unixjoin conf["VIRTUOSO_DATA_DIR"], conf["VIRTUOSO_SQL_SCRIPT"]
 
       #triplestore.rb
 
       #create_w3id.rb
-      conf["W3ID_LOCAL_DIR"] = conf["TOP_OUTPUT_DIR"] + "w3id/"
-      conf["HTACCESS"] = conf["W3ID_LOCAL_DIR"] + ".htaccess"
+      conf["W3ID_LOCAL_DIR"] = join conf["TOP_OUTPUT_DIR"], "w3id", ""
+      conf["HTACCESS"] = join conf["W3ID_LOCAL_DIR"], ".htaccess"
       conf["W3ID_REMOTE_SSH"] = "#{conf["DEPLOYMENT_SERVER"]}:#{conf["W3ID_REMOTE_LOCATION"]}#{conf["URI_PATH_PREFIX"]}"
       conf["REDIRECT_W3ID_TO"] = "#{conf["URI_SCHEME"]}://#{conf["SERVER_ALIAS"]}/#{conf["URI_PATH_PREFIX"]}"
       #create_w3id.rb
@@ -89,13 +104,15 @@ module SeOpenData
 
       #end config
 
-      # make that dir
-      system("mkdir -p " + conf["GEN_CSV_DIR"])
-      system("mkdir -p " + conf["GEN_CSS_DIR"])
-      system("mkdir -p " + conf["GEN_VIRTUOSO_DIR"])
-      system("mkdir -p " + conf["GEN_SPARQL_DIR"])
-      system("mkdir -p " + conf["W3ID_LOCAL_DIR"])
-
+      # Make sure these dirs exist
+      FileUtils.mkdir_p conf.fetch_values(
+        'GEN_CSV_DIR',
+        'GEN_CSS_DIR',
+        'GEN_VIRTUOSO_DIR',
+        'GEN_SPARQL_DIR',
+        'W3ID_LOCAL_DIR'
+      )
+      
       @config_map = conf
     end
     
