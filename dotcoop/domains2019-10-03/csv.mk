412577f7316a8fd3cf291421c57d813d8aa628b4
Nick Stokoe <git.wu-lee@noodlefactory.co.uk>
2020-06-12 00:13:01 +0100

    se_open_data/csv/open_cage_standard.rb - allow the API key to be specified
    ...rather than hard-wiring the filename in the repo (it's not there
    any more, instead it's kept in the password-store repository and
    accessed via `pass`).
    
    In the makefiles this is somewhat of a hack, but they should be being
    retired soonish I hope.

--- dotcoop/domains2019-10-03/csv.mk
+++ dotcoop/domains2019-10-03/csv.mk
@@ -91,9 +91,10 @@ $(STD_DE_DUPED_CSV) : $(STD_CLEAN_UK_CSV)  | $(GEN_CSV_DIR)
 	@echo "gets here"
 
 # Populate the container lat/long fields, where the postcodeunit is the container:
+# `OpenCageKey.txt` is passed to `pass`, change this if the password is stored with a different name
 $(STD_CORRECTED_GEO) : $(STD_DE_DUPED_CSV) | $(GEN_CSV_DIR)
 #$(STANDARD_CSV) : $(STD_CLEAN_UK_CSV) | $(GEN_CSV_DIR)
-	$(RUBY) $(CSV_POSTCODEUNIT_ADDER) --postcodeunit-cache $(POSTCODE_LAT_LNG_CACHE) --postcode-global-cache $(POSTCODE_LAT_LNG_GLOBAL_CACHE) $< > $@
+	$(RUBY) $(CSV_POSTCODEUNIT_ADDER) --pass OpenCageKey.txt --postcodeunit-cache $(POSTCODE_LAT_LNG_CACHE) --postcode-global-cache $(POSTCODE_LAT_LNG_GLOBAL_CACHE) $< > $@
 
 # Create 'clean' derivative of standard
 $(STANDARD_CSV) : $(STD_CORRECTED_GEO)  | $(GEN_CSV_DIR)
