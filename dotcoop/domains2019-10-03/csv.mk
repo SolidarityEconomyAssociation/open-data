f65d43afbcfbcebe41c86f1b6dd1cdde4b0e08c0
Dean <dean.samarjiev@gmail.com>
2020-08-05 08:08:31 +0100

    added geocoding-document generation

--- dotcoop/domains2019-10-03/csv.mk
+++ dotcoop/domains2019-10-03/csv.mk
@@ -27,7 +27,7 @@ CSV_target := csv
 
 # Source files:
 #CLEAN_UK_CSV := $(SRC_CSV_DIR)2019-10-03_DotCoop_Merged.csv
-CLEAN_UK_CSV := $(SRC_CSV_DIR)export20200730.csv
+CLEAN_UK_CSV := $(SRC_CSV_DIR)export.csv
 
 # Here's the directory where we generate intermediate csv files
 GEN_CSV_DIR := $(TOP_OUTPUT_DIR)csv/
