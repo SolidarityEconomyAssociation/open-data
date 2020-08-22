3d9b34c94fbff4dc606e57d633ffc86984a3a5a3
Nick Stokoe <nick.stokoe@controllis.com>
2020-08-22 17:41:23 +0100

    co-ops-uk/2019-06/editions/* - replace final, test etc. with dev, prod
    These deploy to the dev server and the production server.
    
    Use https://(dev.)lod.coop/coops-uk PURLs

--- co-ops-uk/2019-06/editions/prod.mk
+++ co-ops-uk/2019-06/editions/prod.mk
@@ -9,7 +9,7 @@ SRC_CSV_DIR := co-ops-uk-csv-data/
 # Generated URIs will datrt with: $(URI_SCHEME)://$(URI_HOST)/$(URI_PATH_PREFIX)
 URI_SCHEME := https
 URI_HOST := lod.coop
-URI_PATH_PREFIX := coops-uk/2019/
+URI_PATH_PREFIX := coops-uk
 
 ###########################################
 # To where are the Linked Data to be deployed?
@@ -17,7 +17,8 @@ URI_PATH_PREFIX := coops-uk/2019/
 # can be dereferenced to the deployed RDF and HTML files (configured below).
 # The value of DEPLOYMENT_SERVER should be the name of a host set up in an ssh config file. 
 #     (See http://nerderati.com/2011/03/17/simplify-your-life-with-an-ssh-config-file/)
-DEPLOYMENT_SERVER ?= ise-0-matt
+DEPLOYMENT_SERVER ?= sea-0
+ESSGLOBAL_URI	 := https://w3id.solidarityeconomy.coop/essglobal/V2a/
 
 # The directory on the deployment server where the RDF and HTML is to be deployed:
 DEPLOYMENT_WEBROOT := /var/www/html/data1.solidarityeconomy.coop/
@@ -31,7 +32,7 @@ DEPLOYMENT_RSYNC_FLAGS := --delete
 # Set up the triplestore:
 #
 # virtuoso server name, typically this is configured in ~/.ssh/config:
-VIRTUOSO_SERVER := ise-0-admin
+VIRTUOSO_SERVER := sea-0
 # Directory on virtuoso server which has been configured (DirsAllowed in virtuoso.ini)
 # ready for Bulk data loading:
 VIRTUOSO_ROOT_DATA_DIR := /home/admin/Virtuoso/BulkLoading/Data/
