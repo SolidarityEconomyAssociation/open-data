4c20b3646a75da73521bb787b2417340bb89146d
Nick Stokoe <git.wu-lee@noodlefactory.co.uk>
2020-08-31 23:55:05 +0100

    co-ops-uk/2019-06/editions/dev.mk - fixup

--- co-ops-uk/2019-06/editions/dev.mk
+++ co-ops-uk/2019-06/editions/dev.mk
@@ -8,7 +8,7 @@ SRC_CSV_DIR := co-ops-uk-csv-data/
 # Components of the URIs used in this dataset:
 # Generated URIs will datrt with: $(URI_SCHEME)://$(URI_HOST)/$(URI_PATH_PREFIX)
 URI_SCHEME := https
-URI_HOST := lod.coop
+URI_HOST := dev.lod.coop
 URI_PATH_PREFIX := coops-uk
 
 ###########################################
