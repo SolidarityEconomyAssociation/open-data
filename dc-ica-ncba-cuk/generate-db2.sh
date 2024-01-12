75348cec320d641c880a8e135fc62e2963828dc3
Nick Stokoe <git.wu-lee@noodlefactory.co.uk>
2024-01-12 14:17:20 +0000

    dc-ica-ncba-cuk/dump-csv.sh - move the table query into generate-db2.sh
    As a view. This means we can inspect it there.

--- dc-ica-ncba-cuk/generate-db2.sh
+++ dc-ica-ncba-cuk/generate-db2.sh
@@ -290,3 +290,62 @@ union
 select * from ica_not_dc_orgs
 ;"
 
+# Create the final view for export as map data
+#
+# NOTE: we deliberately select the Identifier preferentially from ica,
+# ncba, dc - rather than ica, dc, ncba as for the other fields.  This
+# is because of a duplicate org match resulting in NCBA's orgs, ID
+# 8227012210 and 8227153053, to match the same DotCoop org ID VKoYjW.
+# This is a hacky workaround!
+sqlite3 $DB <<'EOF'
+create view map_data as
+select 
+  'demo/plus/'||coalesce(ica.Identifier, ncba.Identifier, dc.Identifier) as Identifier,
+  coalesce(ica.Name, dc.Name, ncba.Name) as Name,
+  coalesce(ica.Description, dc.Description, ncba.Description) as Description,
+  coalesce(ica.Website, dc.Website, ncba.Website) as Website,
+  coalesce(ica.Latitude, dc.Latitude, ncba.Latitude) as Latitude,
+  coalesce(ica.Longitude, dc.Longitude, ncba.Longitude) as Longitude,
+  coalesce(ica.`Geo Container Latitude`, dc.`Geo Container Latitude`, ncba.`Geo Container Latitude`) as `Geo Container Latitude`,
+  coalesce(ica.`Geo Container Longitude`, dc.`Geo Container Longitude`, ncba.`Geo Container Longitude`) as `Geo Container Longitude`,
+  coalesce(ica.`Geo Container`, dc.`Geo Container`, ncba.`Geo Container`) as `Geo Container`,
+
+  ncba.Identifier as `NCBA Identifier`,
+  ncba.Postcode as `NCBA Postcode`,
+  ncba.City as `NCBA City`,
+  ncba.`State/Region` as `NCBA State/Region`,
+  ncba.`Country ID` as `NCBA Country ID`,
+  ncba.`Co-op Sector` as `NCBA Co-op Sector`,
+  ncba.Industry as `NCBA Industry`,
+  ncba.`Identifier` is not null as `NCBA Member`,
+
+  ica.`Identifier` as `ICA Identifier`, 
+  ica.`Name` as `ICA Name`,
+  ica.`Street Address` as `ICA Street Address`,
+  ica.`Locality` as `ICA Locality`,
+  ica.`Region` as `ICA Region`,
+  ica.`Postcode` as `ICA Postcode`,
+  ica.`Country ID` as `ICA Country ID`,
+  ica.`Website` as `ICA Website`,
+  ica.`Primary Activity` as `ICA Primary Activity`,
+  ica.`Activities` as `ICA Activities`,
+  ica.`Organisational Structure` as `ICA Organisational Structure`,
+  ica.`Membership Type` as `ICA Typology`,
+  ica.`Identifier` is not null as `ICA Member`,
+  
+  dc.`Identifier` as `DC Identifier`, 
+  dc.`Name` as `DC Name`,
+  dc.`Street Address` as `DC Street Address`,
+  dc.`Locality` as `DC Locality`,
+  dc.`Region` as `DC Region`,
+  dc.`Postcode` as `DC Postcode`,
+  dc.`Country ID` as `DC Country ID`,
+  dc.Domains as Domains,
+  dc.`Economic Sector ID` as `DC Economic Sector`,
+  dc.`Organisational Category ID` as `DC Organisational Category`,
+  dc.`Identifier` is not null as `DC Registered`
+from all_orgs
+left join dc on all_orgs.dcid = dc.Identifier
+left join ncba on all_orgs.ncbaid = ncba.Identifier
+left join ica on all_orgs.icaid = ica.Identifier
+EOF
