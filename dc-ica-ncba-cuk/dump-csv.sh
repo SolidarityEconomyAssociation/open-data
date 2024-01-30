64d5475d4b3bc10c02d92d361d51b5e1eea5eb93
Nick Stokoe <git.wu-lee@noodlefactory.co.uk>
2024-01-30 12:05:20 +0000

    dc-ica-ncba-cuk/dump-csv.sh- minor comment alteration

--- dc-ica-ncba-cuk/dump-csv.sh
+++ dc-ica-ncba-cuk/dump-csv.sh
@@ -17,6 +17,6 @@ set -vx
 
 . defs.sh
 
-# Dump all_orgs - all NCBA/ICA/DC organsiations, linked by .coop domains where possible
+# Dump map_data - all NCBA/ICA/DC organsiations, linked by .coop domains where possible
 # Add in extra information from original datasets.
 sqlite3 $DB -csv -header >ncba-dc-ica.csv "select * from map_data"
